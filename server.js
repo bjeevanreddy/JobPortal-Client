5e7a844867b53714c82c342972726dbdb4a84503
jeevan <bjeevanreddy45@gmail.com>
2020-05-05 23:08:56 +0530

    build folder

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/jobportal'));
+app.use(express.static('./dist/portal'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/jobportal/index.html'));
+res.sendFile(path.join(__dirname,'/dist/portal/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
